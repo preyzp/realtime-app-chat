7792eaa1651682f9b9e63999fb82055fbcac9389
Zakharias Priya <priyazakharias@gmail.com>
2023-04-28 00:14:16 +0700

    update dbts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,6 +1,6 @@
 import { Redis } from "@upstash/redis";
 
 export const db = new Redis({
-  url: process.env.NEXT_PUBLIC_UPSTASH_REDIS_REST_URL,
-  token: process.env.NEXT_PUBLIC_UPSTASH_REDIS_REST_TOKEN,
+  url: <string>process.env.NEXT_PUBLIC_UPSTASH_REDIS_REST_URL,
+  token: <string>process.env.NEXT_PUBLIC_UPSTASH_REDIS_REST_TOKEN,
 });
